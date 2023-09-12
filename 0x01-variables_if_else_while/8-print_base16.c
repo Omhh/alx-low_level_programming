a3366babdb85f9a100ea865fda35a9b625a7f10b
Omhh1 <omeryasser91001@gmail.com>
2023-09-11 17:22:24 -0700

    df

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print alphapet in lowercase
+ *
+ * Return: Always zero (success)
+ */
+
+int main(void)
+{
+	int n = 0;
+	char b = 'a';
+
+	while(n <= 9)
+{
+		putchar(n + '0');
+		n++;
+}	
+	while(b <= 'f')
+{
+		putchar(b);
+		b++;
+}
+	putchar('\n');
+
+	return (0);
+}
