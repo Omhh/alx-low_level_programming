58ffde106b108d9502fc0a7f5979aba4ba729a09
Omhh1 <omeryasser91001@gmail.com>
2023-09-12 15:25:22 -0700

    g

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print alphapet in lowercase
+ *
+ * Return: Always zero (success)
+ */
+
+int main(void)
+{
+	putchar('_');
+	printf("putchar\n");
+
+	return (0);
+}
