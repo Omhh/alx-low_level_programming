95afb2a186daa392d88a4948ab19602ac9c62d55
Omhh1 <omeryasser91001@gmail.com>
2023-09-15 09:27:05 -0700

    hhh

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,14 +4,14 @@
 /**
  * print_most_numbers - prints numbers from 1 to 9 except 2 and 4
  *
- * 
+ *
  *
  */
 
 void print_most_numbers(void)
 {
 	int n = 0;
-	
+
 	for (; n <= 9; n++)
 	{
 		if (n == 2 || n == 4)
