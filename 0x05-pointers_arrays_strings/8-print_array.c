b3a3153a20be9c4f90ce30eb9215f0f3819d1158
Omhh1 <omeryasser91001@gmail.com>
2023-09-19 19:49:32 -0700

    ghg

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers
+ *
+ * @n: input
+ *
+ * @a: input
+ *
+ * Return: nothing
+ */
+
+void print_array(int *a, int n)
+{
