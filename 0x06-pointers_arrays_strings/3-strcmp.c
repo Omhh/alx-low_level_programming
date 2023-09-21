f14f7712cd06e75d4e08cf2d0f5bbc79f1b8a42d
Omhh1 <omeryasser91001@gmail.com>
2023-09-22 00:01:54 +0300

    asddfg

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,13 +4,15 @@
  * _strcmp - compares two strings.
  * @s1: input
  * @s2: input
- * Return: 
+ * Return: 1 if true , 0 if false
 */
 int _strcmp(char *s1, char *s2)
 {
 int a = 0;
+
 while (*s1)
 {
+
 if (*s1 != *s2)
 {
 a = ((int)*s1 - 48) - ((int)*s2 - 48);
