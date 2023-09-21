0a36dffa58817d061d4f3f60264f51d8ecea5d32
Omhh1 <omeryasser91001@gmail.com>
2023-09-21 21:23:00 +0300

    a

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,5 +11,13 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
+int a, b;
+a = 0;
+while (dest[a])
+a++;
 
+for (b = 0; b < n && src[b] != '\n'; b++)
+dest[a + b] = src[b];
+dest[a + b] = '\0';
+return (dest);
 }
