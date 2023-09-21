507a4a996ef818e8631a956a44a62921374dba7e
Omhh1 <omeryasser91001@gmail.com>
2023-09-21 23:58:55 +0300

    asd

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings.
+ * @s1: input
+ * @s2: input
+ * Return: 
+*/
+int _strcmp(char *s1, char *s2)
+{
+int a = 0;
+while (*s1)
+{
+if (*s1 != *s2)
+{
+a = ((int)*s1 - 48) - ((int)*s2 - 48);
+break;
+}
+s1++;
+s2++;
+}
+return (a);
+}
