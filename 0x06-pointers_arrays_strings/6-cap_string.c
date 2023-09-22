6df18692ad4b075bdf91c2f1dd7d1a9df8187a77
Omhh1 <omeryasser91001@gmail.com>
2023-09-22 05:59:19 +0300

    yui

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -19,12 +19,14 @@ return (c >= 97 && c <= 122);
 int isDelimiter(char c)
 {
 int i;
-char delimiter[] = "\t\n,.!?\"[]{}";
+char delimiter[] = " \t\n,.!?\"(){}";
 
 for (i = 0; i < 12; i++)
 
 if (c == delimiter[i])
+{
 return (1);
+}
 
 return (0);
 }
