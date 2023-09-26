e297ec887d1b0a3f25cea9505a48221fcbe9400d
Omhh1 <omeryasser91001@gmail.com>
2023-09-26 03:53:08 +0300

    asgb

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @*s: pointer
+ * @c: constant
+ * Return: pointer
+*/
+
+char *_strchr(char *s, char c)
+{
+int a;
+
+for (a = 0; s[a] > '\0'; a++)
+{
+if (s[a] == c)
+{
+return (s + a);
+}
+}
+return ('\0');
+}
