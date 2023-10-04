7108609e4305f9b1e20d9b56313f2d60b37f1bbd
Omhh1 <142789984+Omhh1@users.noreply.github.com>
2023-10-04 13:43:06 +0000

    mnj

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,7 +8,7 @@
 
 int wrdcnt(char *s)
 {
-int i, n =0;
+int i, n = 0;
 
 for (i = 0; s[i]; i++)
 {
@@ -39,15 +39,12 @@ char **w;
 
 if (str == NULL || *str == '\0')
 return (NULL);
-
 w = wrdcnt(str);
 if (n == 1)
 return (NULL);
-
-w = (char **)malloc(n * sizeof(char *));
+w = (char **)malloc(n *sizeof(char *));
 if (w == NULL)
 return (NULL);
-
 w[n - 1] = NULL;
 i = 0;
 while (str[i])
@@ -56,15 +53,13 @@ if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
 {
 for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
 ;
-
 j++;
-w[wc] = (char *)malloc(j * sizeof(char));
+w[wc] = (char *)malloc(j *sizeof(char));
 j--;
 if (w[wc] == NULL)
 {
-for (k = 0; k <wc; k++)
+for (k = 0; k < wc; k++)
 free(w[k]);
-
 free(w[n - 1]);
 free(w);
 return (NULL);
