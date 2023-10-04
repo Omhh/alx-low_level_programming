cf3218d3450c8e63f2aa0b796a629a0d13fdf595
Omhh1 <142789984+Omhh1@users.noreply.github.com>
2023-10-04 03:57:36 +0000

    r

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints its name, followed by a new line
+ * @argc: int
+ * @argv: list
+ * Return: 0 (success)
+*/
+
+int main(int argc, char const *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
