a7702273cf7822b4bae19f221bdbcd8d2dbb001d
Omhh1 <142789984+Omhh1@users.noreply.github.com>
2023-10-11 23:55:14 +0300

    qwert

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_pname - prints a name
+ * @name: string name
+ * @f: printing function pointer
+ * Return: void
+*/
+
+void print_pname(char *name, void (*f)(char *))
+{
+if (name&& f)
+f(name);
+}
