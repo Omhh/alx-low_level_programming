cf5135ed8aad9dc397dc113019f2bb4b1e3dcc66
Omhh1 <142789984+Omhh1@users.noreply.github.com>
2023-10-11 12:32:49 +0000

    bcc

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,6 +7,7 @@
  * @age: dog's age
  * @owner: owner's name
  * Description: dog struct
+ * init dog - initializes a dog
 */
 
 struct dog
@@ -15,5 +16,7 @@ char *name;
 float age;
 char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
